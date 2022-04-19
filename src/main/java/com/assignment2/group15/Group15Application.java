c6e9e78706e970272da07130704530b17bdf0764
tuan07 <s3817907@rmit.edu.vn>
2022-04-19 21:55:52 +0700

    first commit

---
+++ src/main/java/com/assignment2/group15/Group15Application.java
@@ -0,0 +1,13 @@
+package com.assignment2.group15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group15Application.class, args);
+	}
+
+}
